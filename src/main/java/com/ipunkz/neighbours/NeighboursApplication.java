d58b0d17e81a89be98d16c05214fe3c043b38139
Gergely Nagy <gergely.nagy1205@gmail.com>
2019-10-25 10:24:48 +0200

    project configured

---
+++ src/main/java/com/ipunkz/neighbours/NeighboursApplication.java
@@ -0,0 +1,13 @@
+package com.ipunkz.neighbours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighboursApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NeighboursApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ipunkz/neighbours/NeighboursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipunkz.neighbours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighboursApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
