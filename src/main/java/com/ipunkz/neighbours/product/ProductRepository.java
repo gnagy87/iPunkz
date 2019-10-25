3d35055295bd32f18f16a3378e768e1d552ad73b
Gergely Nagy <gergely.nagy1205@gmail.com>
2019-10-25 21:47:24 +0200

    auction endpoint work with necessary refactors

--- src/main/java/com/ipunkz/neighbours/product/ProductRepository.java
+++ src/main/java/com/ipunkz/neighbours/product/ProductRepository.java
@@ -3,6 +3,10 @@ package com.ipunkz.neighbours.product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+  List<Product> findAll();
+  List<Product> findAllByShortDescriptionContaining(String search);
 }
